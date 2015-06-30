0366c69e1f4145cec10b48f169f8b6de3feba374
David Perez <davidperezvalenciano@gmail.com>
2015-06-30 14:07:18 +0200

    Busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,11 +15,21 @@ exports.load = function(req,res,next,quizId){
 
 //GET /quizes
 exports.index = function(req,res){
-	models.Quiz.findAll().then(
-		function(quizes){
-		 res.render('quizes/index',{quizes:quizes});
-		}
-	).catch(function(error){ next(error);})
+	if(req.query.busqueda){
+		models.Quiz.findAll({where: ["pregunta like ?", '%' + req.query.busqueda + '%']}).then(
+			function(quizes){
+				res.render('quizes/index',{quizes:quizes});
+			}
+		).catch(function(error){next(error);})
+	}
+	else{
+		models.Quiz.findAll().then(
+			function(quizes){
+		 	  res.render('quizes/index',{quizes:quizes});
+			}
+		).catch(function(error){ next(error);})
+	}
+	
 };
 
 //GET /quizes/:id
