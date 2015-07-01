8dde6cc833729e152d253d657684361904a4e4ad
David Perez <davidperezvalenciano@gmail.com>
2015-07-01 19:58:29 +0200

    Validaciones

--- models/quiz.js
+++ models/quiz.js
@@ -1,8 +1,16 @@
 //Definición del modelo Quiz
 
 module.exports = function(sequelize,DataTypes){
-	return sequelize.define('Quiz',
-			{ pregunta: DataTypes.STRING,
-			  respuesta: DataTypes.STRING,		
-			});
+	return sequelize.define(
+		'Quiz',
+		{ pregunta: {
+			type: DataTypes.STRING,
+			validate: {notEmpty:{msg: "-> Falta Pregunta"}}
+		},
+		  respuesta: {
+		  	type: DataTypes.STRING,		
+		  	validate: {notEmpty:{msg: "-> Falta Respuesta"}}
+		  }
+		}
+	);
 }
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController= require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz' ,errors:[]});
 });
 
 //Autoload de comandos con quizId
@@ -20,7 +20,7 @@ router.post('/quizes/create',quizController.create);
 
 
 router.get('/author',function(req,res){
-	res.render('author',{autor:'David Pérez Valenciano'})
+	res.render('author',{autor:'David Pérez Valenciano',errors:[]})
 });
 
 module.exports = router;
